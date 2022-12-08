a0a31019c49151ffbb6619b27153d0e973384b44
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2022-12-08 21:12:29 +0400

    Add project files.

---
+++ Ref_Out_In_HomeWork/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        int c = 5;//Trberutyun method kancheluc petqa metodic durs arjeqavorvi
+        MyMethod(7, 6, ref c);
+        Console.WriteLine(c);
+        int n;//Chi pahanjum arjeqavorum ,vorovhetev partadira ira nersum arjeqavori
+        My2Method(5, out n);
+        Console.WriteLine(n);
+    }
+    static void MyMethod(int a, int b, ref int c)
+    {
+        c = c + 1;
+    }
+
+    static void My2Method(int d, out int n)
+    {
+        n = 5;
+        n = n + 1;
+    }
+}
